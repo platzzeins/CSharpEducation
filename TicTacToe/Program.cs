635253825f6fdffd080285a274cd205f15f5899e
Nikita Kuznecov <m.h.kuznetsov@student.khai.edu>
2023-07-05 21:14:07 +0300

    Small Fixes

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -36,12 +36,6 @@
 
         private static bool IsEndSituation(TicTacToe ticTacToe, GameUI gameUi)
         {
-            if (ticTacToe.IsDraw())
-            {
-                gameUi.PrintDrawEnd();
-                return true;
-            }
-                
             var lineWinner = ticTacToe.IterateThroughLines();
             var columnWinner = ticTacToe.IterateThroughColumns();
             var diagonalWinner = ticTacToe.IterateThroughDiagonals();
@@ -61,6 +55,12 @@
             if (diagonalWinner != "None")
             {
                 gameUi.PrintWinEnd(diagonalWinner);
+                return true;
+            }
+            
+            if (ticTacToe.IsDraw())
+            {
+                gameUi.PrintDrawEnd();
                 return true;
             }
 
